37abfd114957e469af4dae2b1c50ab7818df399b
dinesh0707 <38342114+dinesh0707@users.noreply.github.com>
2018-08-14 11:35:44 +0530

    Add files via upload
    c file

---
+++ 1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter a character:");
+scanf("%c",&a);
+if
+(
+a='a','e','i','o','u';
+
+printf("it is a vowel");
+)
+else
+(
+printf("it is not a vowel");
+}
+}
+
